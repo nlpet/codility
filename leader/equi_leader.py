505320966cca28a068d3504dd01a263081bb1b3c
Nora Petrova <schottkey@gmail.com>
2015-09-13 00:50:17 +0100

    Update equi_leader.py

--- leader/equi_leader.py
+++ leader/equi_leader.py
@@ -1,44 +1,52 @@
 """
 https://codility.com/demo/take-sample-test/equi_leader/
 """
+import unittest
+
+
+class TestEquiLeader(unittest.TestCase):
+
+    """Unit tests for max product of triples."""
+
+    def tests(self):
+        """Tests."""
+        self.assertEqual(solution([1, 2]), 0)
+        self.assertEqual(solution([0]), 0)
+        self.assertEqual(solution([4, 3, 4, 4, 4, 2]), 2)
+        self.assertEqual(solution([4, 4, 2, 5, 3, 4, 4, 4]), 3)
 
 
 def solution(A):
-    doms = {}
-    dom = [0, 0]
-    length = len(A)
-    for i in range(length):
-        try:
-            doms[A[i]][0] += 1
-            doms[A[i]][1].append(i)
-            doms[A[i]][2] += 1
-            if doms[A[i]][2] > dom[0]:
-                dom[0] = A[i]
-                dom[1] = doms[A[i]][2]
-        except (KeyError, IndexError):
-            doms[A[i]] = [1, [i], 1]
+    dom = 0
+    count = 0
+    N = len(A)
 
-    
-    leader = doms[dom[0]]
-    # Number of leadrs in the first half
-    fst = 0
-    # Number of leaders in the second half
-    snd = leader[0]
+    for i in range(N):
+        if count == 0:
+            dom = i
+        if A[i] == A[dom]:
+            count += 1
+        else:
+            count -= 1
 
-    solutions = 0
+    count = len([i for i in range(N) if A[i] == A[dom]])
+    if count <= (N / 2):
+        return 0
 
-    for i in leader[1]:
-        fst += 1
-        snd -= 1
-        # Number of non-leaders in the first half
-        fst_non = (i + 1) - fst
-        # Number of non-leaders in the second half
-        snd_non = length - (i + 1) - snd
-        if fst > fst_non and snd > snd_non:
-            solutions += 1
-    return solutions
+    leader = A[dom]
+    equis = 0
+    first_half, second_half = 0, count
+
+    for j in range(N):
+        if A[j] == leader:
+            first_half += 1
+            second_half -= 1
+
+        if first_half > (j + 1) / 2 and second_half > (N - j - 1) / 2:
+            equis += 1
+
+    return equis
 
 
 if __name__ == '__main__':
-    A = [4, 3, 4, 4, 4, 2]
-    print solution(A)
+    unittest.main()
